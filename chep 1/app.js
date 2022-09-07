9ed425212dbfdf642aef23298777c601dc076e11
ali <syedaliibrahim50@gmail.com>
2022-09-08 00:54:55 +0500

    Initial commit

---
+++ chep 1/app.js
@@ -0,0 +1,3 @@
+alert("error! please enter a valid password.");
+
+alert("welome to js land....\n happy  coding!");
---
+++ chep 2/aap.js
@@ -0,0 +1,5 @@
+alert("ali");
+alert(" 15 year old ");
+alert (" certifield mobile application development");
+alert ("my email adreess i exp@.com");
+alert("i am trying to learn from the books /br a smarter way to javascript");
